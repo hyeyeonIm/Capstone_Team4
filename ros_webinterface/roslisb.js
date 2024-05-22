a2458f94f79c3c7880fd1e9437912b67cf671167
hyeyeonIm <enhyeonii@gmail.com>
2024-05-22 14:33:37 +0900

    web_ui

--- ros_webinterface/roslisb.js
+++ ros_webinterface/roslisb.js
@@ -9,11 +9,45 @@ $(document).ready(function() {
     ros.on('connection', function() {
         console.log('Connected to websocket server.');
         statusSpan.innerHTML = "Connected";
+
+        var roomMarkerListener = new ROSLIB.Topic({
+            ros: ros,
+            name: '/room_markers',
+            messageType: 'visualization_msgs/Marker'
+        });
+
+        console.log('Attempting to subscribe to /room_markers');
+
+        roomMarkerListener.subscribe(function(message) {
+            console.log('Received message on ' + roomMarkerListener.name + ': ', message);
+            displayMarker(message);
+        });
+
+
+        // 로봇 위치 구독 추가
+     var robotPoseListener = new ROSLIB.Topic({
+        ros: ros,
+        name: '/pose',
+        messageType: 'geometry_msgs/Pose'
     });
+
+    robotPoseListener.subscribe(function(message) {
+        var x = message.position.x * 50 + 400; // 스케일링 및 중심 위치 조정
+        var y = -message.position.y * 50 + 300; // 스케일링 및 중심 위치 조정
+        var quaternion = message.orientation;
+        var theta = Math.atan2(2.0 * (quaternion.w * quaternion.z + quaternion.x * quaternion.y),
+                               1.0 - 2.0 * (quaternion.y * quaternion.y + quaternion.z * quaternion.z));
+        updateRobotPosition(x, y, theta);
+    });
+
+
+    });
+
     ros.on('error', function(error) {
-        console.log('Error connecting to websocket server: ', error);
+        console.log('Error connecting to websocket server:', error);
         statusSpan.innerHTML = "Error";
     });
+
     ros.on('close', function() {
         console.log('Connection to websocket server closed.');
         statusSpan.innerHTML = "Closed";
@@ -23,10 +57,10 @@ $(document).ready(function() {
     // 페이지 로드 시 매핑 상태 확인
     if(localStorage.getItem("mappingFinished") === "true") {
         $(".Start_Map, .Stop_Map").hide();
-        $(".Mapping_Again").show();
+        $(".Mapping_Again, .Segmenatation").show();
     } else {
         $(".Start_Map, .Stop_Map").show();
-        $(".Mapping_Again").hide();
+        $(".Mapping_Again, .Segmenatation").hide();
     }
 
     // 매핑 시작 버튼 클릭 이벤트
@@ -61,6 +95,7 @@ $(document).ready(function() {
         $(".Start_Map").hide();
         $(".Stop_Map").hide();
         $(".Mapping_Again").show();
+        $(".Segmentation").show();
     });
 
     // 다시 매핑하기 버튼 클릭 이벤트
@@ -70,13 +105,64 @@ $(document).ready(function() {
         $(".Start_Map").show();
         $(".Stop_Map").show();
         $(this).hide(); // "다시 매핑하기" 버튼 숨기기
+        //$(".Segmentation").hide(); // "공간 나누기" 버튼 숨기기
     });
+
+    fetchSavedRooms();
+
+    $('.Segmentation').off('click').on('click', async function() {
+        var mapName = prompt("Enter the map name:", "defaultMapName");
+        if (mapName) {
+            try {
+                console.log(`Starting segmentation for map: ${mapName}`);
+                const response = await $.get(`http://localhost:8080/Segmentation/${mapName}`);
+                console.log(response);
+                alert(response);
+
+                // Segmentation 완료 후 room_coordinates.txt 읽기
+                console.log("Reading room coordinates");
+                const roomResponse = await $.get('http://localhost:8080/room_coordinates.txt');
+                console.log("Room coordinates received:", roomResponse);
+                updateRoomList(roomResponse);
+            } catch (error) {
+                console.error("An error occurred:", error);
+                alert(`Error: ${error.statusText}`);
+            }
+        }
+    });
+
+    // Save room info button click event
+    $('.save_room_info').off('click').on('click', function() {
+        var selectedRooms = [];
+        document.querySelectorAll('input[name="rooms"]:checked').forEach((checkbox) => {
+            selectedRooms.push(checkbox.value);
+        });
+
+        if (selectedRooms.length > 0) {
+            $.post('http://localhost:8080/save_coordinates', { rooms: selectedRooms.join(',') })
+                .done(function(response) {
+                    alert("Coordinates saved successfully!");
+                    console.log("Coordinates saved:", response);
+                    location.reload(); // 화면 새로고침
+                })
+                .fail(function(error) {
+                    console.error("Error saving coordinates:", error);
+                    alert("Error saving coordinates.");
+                });
+        } else {
+            alert('No rooms selected');
+        }
+    });
+
+
+
+
 
     // 지도 시각화 관련 코드는 그대로 유지
     var viewer = new ROS2D.Viewer({
         divID: 'map',
-        width: 500,
-        height: 500
+        width: 800,
+        height: 600
     });
 
     var gridClient = new ROS2D.OccupancyGridClient({
@@ -90,8 +176,55 @@ $(document).ready(function() {
         viewer.scaleToDimensions(gridClient.currentGrid.width, gridClient.currentGrid.height);
         viewer.shift(gridClient.currentGrid.pose.position.x, gridClient.currentGrid.pose.position.y);
     });
+
+    function displayMarker(marker) {
+        // 선택된 Room ID만 표시
+        const markerRoomID = marker.text.replace('Room ', '').trim();
+        console.log("Displaying marker for room: ", markerRoomID);
+        console.log("Current selected rooms: ", window.selectedRooms);
+
+        if (window.selectedRooms && window.selectedRooms.includes(markerRoomID)) {
+            var shape = new createjs.Shape();
+            shape.graphics.beginFill("blue").drawCircle(0, 0, 0.1);
+            shape.x = marker.pose.position.x;
+            shape.y = -marker.pose.position.y;
+
+            viewer.scene.addChild(shape);
+
+            var text = new createjs.Text(marker.text, "0.5px Arial", "#005F73");
+            text.x = marker.pose.position.x;
+            text.y = -marker.pose.position.y + 0.1;
+
+            viewer.scene.addChild(text);
+        } else {
+            console.log(`Marker for Room ${markerRoomID} not displayed.`);
+        }
+    }
+
+
+    // 로봇 위치 업데이트 함수
+    function updateRobotPosition(x, y, theta) {
+        var canvas = document.getElementById("robotCanvas");
+        var context = canvas.getContext("2d");
+
+        context.clearRect(0, 0, canvas.width, canvas.height);
+
+        var robotIcon = new Image();
+        robotIcon.src = "./icons/buddy_icon.png";
+        robotIcon.onload = function() {
+            context.save();
+            context.translate(x, y);
+            context.rotate(theta);
+            context.drawImage(robotIcon, -5, -5, 20, 20); 
+            context.restore();
+        };
+    }
+
+
+
 });
 
+    
 
 
     // // Example of subscribing to a topic
--- ros_webinterface/server.py
+++ ros_webinterface/server.py
@@ -1,33 +1,97 @@
 import http.server
 import socketserver
 import subprocess
-from urllib.parse import urlparse, unquote
-import subprocess
+from urllib.parse import urlparse, unquote, parse_qs
+import os
+import signal
+import json
 
 PORT = 8080
+BASE_DIR = "/home/haley/catkin_ws/roomInfo/"
+running_processes = []
 
 class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         parsed_path = urlparse(self.path)
-        path = parsed_path.path
-
-        if path == "/start_hector":
+        path_components = parsed_path.path.split('/')
+        if parsed_path.path == "/start_hector":
             self.start_hector()
-        elif path.startswith("/stop_and_save/"):
-            map_name = path[len("/stop_and_save/"):]
+        elif parsed_path.path.startswith("/stop_and_save/"):
+            map_name = parsed_path.path[len("/stop_and_save/"):]
             self.stop_and_save(map_name)
-        elif path.startswith("/load_map/"):
-            map_name = path[len("/load_map/"):]
+        elif parsed_path.path.startswith("/load_map/"):
+            map_name = parsed_path.path[len("/load_map/"):]
             self.load_map(map_name)
+        elif path_components[1] == "Segmentation":
+            map_name = unquote(path_components[2])
+            self.segmentation(map_name)
+        elif path_components[1] == "room_coordinates.txt":
+            self.serve_room_coordinates()
+        elif path_components[1] == "save_coordinates.txt":
+            self.serve_saved_coordinates()
+        elif path_components[1] == "schedule.txt":
+            self.serve_schedule()
+        elif len(path_components) > 1 and path_components[1] == "Move":
+            roomId = unquote(path_components[2])
+            self.move_to_room(roomId)
         else:
             super().do_GET()
 
+
+
+    def do_POST(self):
+        parsed_path = urlparse(self.path)
+        path_components = parsed_path.path.split('/')
+        if len(path_components) > 1:
+            action = path_components[1]
+            if action == "save_coordinates":
+                content_length = int(self.headers['Content-Length'])
+                post_data = self.rfile.read(content_length)
+                post_params = parse_qs(post_data.decode('utf-8'))
+                rooms = post_params.get('rooms', [None])[0]
+                if rooms:
+                    self.save_coordinates(rooms.split(','))
+                else:
+                    self.send_response(400)
+                    self.end_headers()
+                    self.wfile.write(bytes("No rooms provided", "utf8"))
+            elif action == "execute_command":
+                content_length = int(self.headers['Content-Length'])
+                post_data = self.rfile.read(content_length)
+                post_params = parse_qs(post_data.decode('utf-8'))
+                command = post_params.get('command', [None])[0]
+                if command:
+                    self.execute_command(command)
+                else:
+                    self.send_response(400)
+                    self.end_headers()
+                    self.wfile.write(bytes("No command provided", "utf8"))
+            else:
+                self.send_response(404)
+                self.end_headers()
+
+
+        # elif path.startswith("/roomInfo/"):
+        #     file_path = os.path.join(BASE_DIR, path.lstrip('/'))
+        #     if os.path.isfile(file_path):
+        #         self.send_response(200)
+        #         self.send_header('Content-type', 'text/plain')
+        #         self.end_headers()
+        #         with open(file_path, 'rb') as file:
+        #             self.wfile.write(file.read())
+        #     else:
+        #         self.send_response(404)
+        #         self.send_header('Content-type', 'text/plain')
+        #         self.end_headers()
+        #         self.wfile.write(b'File not found')
+
+
     def start_hector(self):
         try:
-            subprocess.Popen(['roslaunch', 'hector_slam_launch', 'tutorial.launch'])
-            response = "Hector SLAM launch started successfully."
+            subprocess.Popen(['roslaunch', 'omo_r1mini_slam', 'omo_r1mini_slam.launch'])
+            response = "SLAM launch started successfully."
         except FileNotFoundError as e:
-            response = f"Error starting Hector SLAM launch: {e}"
+            response = f"Error starting SLAM launch: {e}"
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
@@ -36,18 +100,18 @@ class Handler(http.server.SimpleHTTPRequestHandler):
     def stop_and_save(self, map_name):
         map_name = unquote(map_name)  # URL 인코딩된 맵 이름을 디코드
         try:
-            # 맵 저장 : subprocess.run 사용
             save_cmd = ['rosrun', 'map_server', 'map_saver', '-f', f'/home/haley/catkin_ws/saveMap/{map_name}']
             subprocess.run(save_cmd, check=True)  # 명령어 완료까지 대기
             response = f"Map saved as {map_name}."
 
-            # hector_slam 튜토리얼 먼저 종료
-            subprocess.call(['pkill', '-f', 'roslaunch hector_slam_launch tutorial.launch'])
-            # 지도가 저장되었으므로 hector_slam 관련 모든 명령어 종료
-            subprocess.call(['kill' '-f' 'hector_slam_launch'])
+            # slam 종료
+            subprocess.call(['rosnode', 'kill', '/omo_r1mini_slam_gmapping'])
+            # 지도가 저장되었으므로 slam 관련 모든 명령어 종료
+            # subprocess.call(['pkill', '-f', 'omo_r1mini_slam_launch'])
+            response += "\nslam is over"
 
         except FileNotFoundError as e:
-            response = f"Error saving map or stopping hector_slam: {e}"
+            response = f"Error saving map or stopping slam: {e}"
         except subprocess.CalledProcessError as e:
             response = f"Error during command execution: {e}"
             
@@ -72,6 +136,270 @@ class Handler(http.server.SimpleHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(bytes(response, "utf8"))
 
+    
+    def segmentation(self, map_name):
+        global running_processes
+        # 기존 프로세스를 종료합니다.
+        for proc in running_processes:
+            try:
+                os.killpg(os.getpgid(proc.pid), signal.SIGTERM)
+            except Exception as e:
+                print(f"Error killing process: {e}")
+        
+        running_processes = []
+
+        map_path = f"/home/haley/catkin_ws/saveMap/{map_name}"
+        try:
+            #subprocess.Popen(['roslaunch', 'ipa_room_segmentation', 'room_segmentation_action_server.launch'])
+            #subprocess.Popen(['rosrun', 'ipa_room_segmentation', 'room_segmentation_client.py', f'{map_path}.pgm', '0.05'])
+            proc1 = subprocess.Popen(['rosrun', 'omo_control', 'my_markers.py'], preexec_fn=os.setsid)
+            proc2 = subprocess.Popen(['roslaunch', 'omo_r1mini_navigation', 'omo_r1mini_navigation.launch', 'map_file:=' + f'{map_path}.yaml'], preexec_fn=os.setsid)
+
+            running_processes.extend([proc1, proc2])
+
+            response = "All segmentation and visualization commands executed."
+        except Exception as e:
+            response = f"Error during command execution: {e}"
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(bytes(response, "utf8"))
+
+    def serve_room_coordinates(self):
+        try:
+            with open(os.path.join(BASE_DIR, 'room_coordinates.txt'), 'r') as file:
+                content = file.read()
+
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(bytes(content, "utf8"))
+        except Exception as e:
+            self.send_response(500)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(bytes(f"Error reading room coordinates: {e}", "utf8"))
+
+    def serve_saved_coordinates(self):
+        try:
+            with open(os.path.join(BASE_DIR, 'save_coordinates.txt'), 'r') as file:
+                content = file.read()
+
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(bytes(content, "utf8"))
+        except Exception as e:
+            self.send_response(500)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(bytes(f"Error reading saved coordinates: {e}", "utf8"))
+
+
+    def save_coordinates(self, selected_rooms):
+        global running_processes
+        try:
+            with open(os.path.join(BASE_DIR, 'room_coordinates.txt'), 'r') as infile:
+                lines = infile.readlines()
+
+            selected_data = []
+            for line in lines:
+                if line.strip() == "" or line.startswith("RoomID"):
+                    continue
+                room_data = line.strip().split(',')
+                if room_data[0] in selected_rooms:
+                    selected_data.append(line.strip())
+
+            # 디버깅 메시지 추가
+            print(f"Selected Data to save: {selected_data}")
+
+            # save_coordinates.txt 파일에 데이터를 저장합니다.
+            save_coordinates_path = os.path.join(BASE_DIR, 'save_coordinates.txt')
+            with open(save_coordinates_path, 'w') as outfile:
+                outfile.write("RoomID, CenterX (meters), CenterY (meters)\n")
+                for data in selected_data:
+                    outfile.write(data + "\n")
+
+            # schedule.txt 파일에 데이터를 저장합니다.
+            schedule_path = os.path.join(BASE_DIR, 'schedule.txt')
+            with open(schedule_path, 'w') as schedule_file:
+                schedule_file.write("RoomID, CenterX (meters), CenterY (meters)\n")
+                for data in selected_data:
+                    schedule_file.write(data + "\n")
+
+            # 디버깅 메시지 추가
+            print(f"schedule.txt file created at: {schedule_path}")
+
+            # 기존 my_markers.py 프로세스를 종료합니다.
+            for proc in running_processes:
+                try:
+                    if 'my_markers.py' in proc.args:
+                        print(f"Attempting to kill process {proc.pid} for my_markers.py")
+                        os.killpg(os.getpgid(proc.pid), signal.SIGTERM)
+                        running_processes.remove(proc)
+                        print("my_markers.py process killed.")
+                except Exception as e:
+                    print(f"Error killing process: {e}")
+
+            # new_markers.py를 실행합니다.
+            try:
+                print("Starting new_markers.py process")
+                new_proc = subprocess.Popen(['rosrun', 'omo_control', 'new_markers.py'], preexec_fn=os.setsid)
+                running_processes.append(new_proc)
+                print("new_markers.py process started.")
+            except Exception as e:
+                print(f"Error starting new_markers.py: {e}")
+
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(bytes("Coordinates saved successfully", "utf8"))
+        except Exception as e:
+            print(f"Exception in save_coordinates: {e}")
+            self.send_response(500)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(bytes(f"Error saving coordinates: {e}", "utf8"))
+
+    def serve_schedule(self):
+        try:
+            with open(os.path.join(BASE_DIR, 'schedule.txt'), 'r') as file:
+                content = file.read()
+
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(bytes(content, "utf8"))
+        except Exception as e:
+            self.send_response(500)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(bytes(f"Error reading schedule: {e}", "utf8"))
+
+    def execute_command(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        try:
+            data = json.loads(post_data)
+            command = data.get('command')
+            if not command:
+                raise ValueError("No command provided")
+
+            print(f"Executing command: {command}")
+            result = subprocess.run(command, check=True, capture_output=True, text=True)
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(bytes(result.stdout, "utf8"))
+        except Exception as e:
+            print(f"Error executing command: {e}")
+            self.send_response(400)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(bytes(f"Error executing command: {e}", "utf8"))
+
+
+    def move_to_room(self, roomId):
+        try:
+            print(f"Moving to room {roomId}")
+            subprocess.Popen(['rosrun', 'omo_control', 'move_to_room.py', roomId], preexec_fn=os.setsid)
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(bytes(f"Moving to room {roomId}", "utf8"))
+        except Exception as e:
+            print(f"Error moving to room: {e}")
+            self.send_response(500)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(bytes(f"Error moving to room: {e}", "utf8"))
+
+
+    # def serve_room_coordinates(self):
+    #     try:
+    #         with open(os.path.join(BASE_DIR, 'room_coordinates.txt'), 'r') as file:
+    #             content = file.read()
+
+    #         self.send_response(200)
+    #         self.send_header('Content-type', 'text/plain')
+    #         self.end_headers()
+    #         self.wfile.write(bytes(content, "utf8"))
+    #     except Exception as e:
+    #         self.send_response(500)
+    #         self.send_header('Content-type', 'text/plain')
+    #         self.end_headers()
+    #         self.wfile.write(bytes(f"Error reading room coordinates: {e}", "utf8"))
+
+    # def serve_saved_coordinates(self):
+    #     try:
+    #         with open(os.path.join(BASE_DIR, 'save_coordinates.txt'), 'r') as file:
+    #             content = file.read()
+
+    #         self.send_response(200)
+    #         self.send_header('Content-type', 'text/plain')
+    #         self.end_headers()
+    #         self.wfile.write(bytes(content, "utf8"))
+    #     except Exception as e:
+    #         self.send_response(500)
+    #         self.send_header('Content-type', 'text/plain')
+    #         self.end_headers()
+    #         self.wfile.write(bytes(f"Error reading saved coordinates: {e}", "utf8"))
+
+    # def save_coordinates(self, selected_rooms):
+    #     try:
+    #         with open(os.path.join(BASE_DIR, 'room_coordinates.txt'), 'r') as infile:
+    #             lines = infile.readlines()
+
+    #         selected_data = []
+    #         for line in lines:
+    #             if line.strip() == "" or line.startswith("RoomID"):
+    #                 continue
+    #             room_data = line.strip().split(',')
+    #             if room_data[0] in selected_rooms:
+    #                 selected_data.append(line.strip())
+
+    #         with open(os.path.join(BASE_DIR, 'save_coordinates.txt'), 'w') as outfile:
+    #             outfile.write("RoomID, CenterX (meters), CenterY (meters)\n")
+    #             for data in selected_data:
+    #                 outfile.write(data + "\n")
+
+    #         self.send_response(200)
+    #         self.send_header('Content-type', 'text/plain')
+    #         self.end_headers()
+    #         self.wfile.write(bytes("Coordinates saved successfully", "utf8"))
+    #     except Exception as e:
+    #         self.send_response(500)
+    #         self.send_header('Content-type', 'text/plain')
+    #         self.end_headers()
+    #         self.wfile.write(bytes(f"Error saving coordinates: {e}", "utf8"))
+
+
+    # navigation
+    # def navigation(self, map_name):
+    #     map_path = f"/home/haley/catkin_ws/saveMap/{map_name}"
+    #     try:
+    #         # 모든 필요한 ROS 명령을 순차적으로 실행
+    #         # "/home/haley/catkin_ws/roomInfo/save_coordinates.txt" 로부터 new_markers 실행
+    #         # subprocess.Popen(['roslaunch', 'omo_r1mini_navigation', 'omo_r1mini_navigation.launch', 'map_file:=' + f'{map_path}.yaml'])
+    #         # Add carebuddy icon on roslisb.js
+    #         response = "Navigation is open"
+    #     except Exception as e:
+    #         response = f"Error during command execution: {e}"
+    #     self.send_response(200)
+    #     self.send_header('Content-type', 'text/plain')
+    #     self.end_headers()
+    #     self.wfile.write(bytes(response, "utf8"))
+
+    # Move_Schedule
+    # Basic : go to 1st location
+    # Case 1 : make schedule.txt copy by save_coordinates.txt
+    # Case 2 : change current location button
+    #           -> move_to_room right after
+    # Case 3: change next location button
+    #           -> Add Room ID on schedule.txt
+    # Robot always move using by "move_to_room.py {ROOM ID}"
+    # And they stay 10s at location
+    # rosrun omo_control move_to_room.py 3
 
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
