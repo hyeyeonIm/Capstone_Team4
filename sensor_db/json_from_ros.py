4bfa5c6b05e7cbbafc4a3b0ade1b3d631edf731b
Mingyu Kim <mattmingyu@gmail.com>
2024-06-16 23:46:14 +0900

    Add files via upload

---
+++ sensor_db/json_from_ros.py
@@ -0,0 +1,30 @@
+from __future__ import print_function
+import roslibpy
+import requests
+import json
+
+client = roslibpy.Ros(host='localhost', port=9090)
+client.run()
+
+
+def callback(message):
+    json_str = message['data']
+    print('Heard talking: ' + json_str)
+
+    try:
+        url = 'http://localhost:8080/data'
+        headers = {'Content-Type': 'application/json'}
+        response = requests.post(url, data=json_str, headers=headers)
+        print('Server response:', response.status_code, response.text)
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+listener = roslibpy.Topic(client, '/matt', 'std_msgs/String')
+listener.subscribe(callback)
+
+try:
+    while True:
+        pass
+except KeyboardInterrupt:
+    client.terminate()
